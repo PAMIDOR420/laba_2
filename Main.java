9ad86d5ff62bbdcc85795ce66ff2e57863991e80
PAMIDOR420 <keneksifire276@gmail.com>
2023-12-22 12:11:36 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.sql.SQLOutput;
+
+//все правильно
+public class Main {
+    public static void main(String[] args) {
+	    MainFrame frame=new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
